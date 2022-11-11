7b64f628cb46f54bf13087187116626d84cd2dca
WANWISAAA <chumpornw6@gmail.com>
2022-11-11 13:30:08 +0700

    update

---
+++ python501.py
@@ -0,0 +1,12 @@
+a = 4 #0000 0100
+b = 14 #0000 1110
+C = 25#0001 1001
+d = 231#1110 0111
+print(bin(a))
+print(bin(b))
+print(bin(C))
+print(bin(d))
+print(bin(a&b)) #100
+print(bin(a|b)) #1110
+print(bin(a^b)) #1010
+print(bin(~a))
